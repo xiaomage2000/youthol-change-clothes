89b7464d40e87c2e1e71a3fd50179398a2da4a1e
xiaomage2000 <d1540076394@gmail.com>
2020-08-21 23:06:25 +0800

    换装功能已经实现

--- pages/main/main.js
+++ pages/main/main.js
@@ -5,6 +5,12 @@ const {
 Page({
   data: {
     src: '',
+    bodySrc: './model/身体1.png',
+    hairSrc: '',
+    eyeSrc: '',
+    mouseSrc: '',
+    coatSrc: '',
+    pantSrc: '',
     bodyArray: ['身体1', '身体2', '身体3'],
     hairArray: ['头发1', '头发2'],
     eyeArray: ['眼睛1', '眼睛2'],
@@ -15,39 +21,50 @@ Page({
 
   //更改picker事件 代码冗余有点多，以后想办法减少下
   bindPickerBodyChange: function (e) {
-    console.log(e.detail.value)
+    var temp = parseInt(e.detail.value) + 1;
     this.setData({
-      bodyIndex: e.detail.value
+      bodyIndex: e.detail.value,
+      bodySrc: './model/身体' + temp + '.png',
     })
   },
   bindPickerHairChange: function (e) {
+    var temp = parseInt(e.detail.value) + 1;
     this.setData({
-      hairIndex: e.detail.value
+      hairIndex: e.detail.value,
+      hairSrc:'./model/头发' + temp + '.png',
     })
   },
   bindPickerEyeChange: function (e) {
+    var temp = parseInt(e.detail.value) + 1;
     this.setData({
-      eyeIndex: e.detail.value
+      eyeIndex: e.detail.value,
+      eyeSrc: './model/眼睛' + temp + '.png',
     })
   },
   bindPickerMouseChange: function (e) {
+    var temp = parseInt(e.detail.value) + 1;
     this.setData({
-      mouseIndex: e.detail.value
+      mouseIndex: e.detail.value,
+      mouseSrc: './model/嘴巴' + temp + '.png',
     })
   },
   bindPickerCoatChange: function (e) {
+    var temp = parseInt(e.detail.value) + 1;
     this.setData({
-      coatIndex: e.detail.value
+      coatIndex: e.detail.value,
+      coatSrc: './model/上衣' + temp + '.png',
     })
   },
   bindPickerPantChange: function (e) {
+    var temp = parseInt(e.detail.value) + 1;
     this.setData({
-      pantIndex: e.detail.value
+      pantIndex: e.detail.value,
+      pantSrc: './model/下衣' + temp + '.png',
     })
   },
 
 
-  
+
   onLoad() {
     this.widget = this.selectComponent('.widget')
   },
