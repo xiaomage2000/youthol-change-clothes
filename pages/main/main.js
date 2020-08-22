4bb69b2c8a8da7f896cd9eb906d25af8fb2d2bae
xiaomage2000 <d1540076394@gmail.com>
2020-08-22 16:45:09 +0800

    bug fix 随机实现 传参优化

--- pages/main/main.js
+++ pages/main/main.js
@@ -11,7 +11,13 @@ Page({
     eyeArray: ['眼睛1', '眼睛2'],
     mouseArray: ['嘴巴1', '嘴巴2'],
     coatArray: ['上衣1', '上衣2'],
-    pantArray: ['下衣1', '下衣2']
+    pantArray: ['下衣1', '下衣2'],
+    bodyIndex: '',
+    hairIndex: '',
+    eyeIndex: '',
+    coatIndex: '',
+    mouseIndex: '',
+    pantIndex: '',
   },
 
   //更改picker事件 代码冗余有点多，以后想办法减少下
@@ -57,7 +63,29 @@ Page({
       pantSrc: '../model/pant' + temp + '.png',
     })
   },
-  goNext: function(e) {
+  randomButton: function (e) {
+    var bodyRandom = 1 + parseInt(Math.random() * 3);
+    var hairRandom = 1 + parseInt(Math.random() * 2);
+    var eyeRandom = 1 + parseInt(Math.random() * 2);
+    var mouseRandom = 1 + parseInt(Math.random() * 2);
+    var coatRandom = 1 + parseInt(Math.random() * 2);
+    var pantRandom = 1 + parseInt(Math.random() * 2);
+    this.setData({
+      bodySrc: '../model/body' + bodyRandom + '.png',
+      hairSrc: '../model/hair' + hairRandom + '.png',
+      eyeSrc: '../model/eye' + eyeRandom + '.png',
+      mouseSrc: '../model/mouse' + mouseRandom + '.png',
+      coatSrc: '../model/coat' + coatRandom + '.png',
+      pantSrc: '../model/pant' + pantRandom + '.png',
+      bodyIndex: bodyRandom - 1,
+      hairIndex: hairRandom - 1,
+      eyeIndex: eyeRandom - 1,
+      mouseIndex: mouseRandom - 1,
+      coatIndex: coatRandom - 1,
+      pantIndex: pantRandom - 1,
+    })
+  },
+  goNext: function (e) {
     var that = this;
     wx.navigateTo({
       url: '../result/result?body=' + that.data.bodySrc + '&hair=' + that.data.hairSrc + "&eye=" + that.data.eyeSrc + "&mouse=" + that.data.mouseSrc + "&coat=" + that.data.coatSrc + "&pant=" + that.data.pantSrc,
